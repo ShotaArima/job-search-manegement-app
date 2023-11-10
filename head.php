87ceb8a6ce3de162b5623ad37f50b10d1d8ace60
ShotaArima <rin.zoo.mamiya12345@gmail.com>
2023-11-10 16:56:58 +0900

    work point / create head.php however page_entrance.php have to fix some codes.

---
+++ head.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <style>
+            .post {
+                border: 1px solid #ccc;
+                padding: 10px;
+                margin: 10px;
+                background-color: #f5f5f5;
+            }
+
+            .post-text {
+                font-size: 16px;
+            }
+
+            .post-time {
+                font-style: italic;
+                color: #888;
+                /* border: 1px solid #ccc; */
+                padding: 10px;
+                margin: 5px;
+            }
+
+            .likes {
+                color: green;
+                /* border: 1px solid #ccc; */
+                padding: 10px;
+                margin: 5px;
+            }
+
+            .container {
+                display: flex;
+                justify-content: space-between; /* 両端に合わせる */
+            }
+
+            .header {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 10px;
+                background-color: #333;
+                color: #fff;
+                position: fixed;
+                top: 0;
+                left: 0;
+                right: 0;
+                z-index: 100;
+            }
+
+            body {
+                margin-top: 100px;
+            }
+
+        </style>
+        <title>page_main</title>
+    </head>
+    <body>
+        <div class="header">
+            <div style="display: flex; align-items: center;">
+                <h1 style="margin-right: 10px;" href="page_main.php">career secretary</h1>
+                <a style="text-decoration: none; color: #bbbbbb; align-self: flex-end;">就活管理アプリ</a>
+            </div>
+
+            <form method="post" action="page_entrance.php">
+                <button type="submit" class="btn btn-primarybtn btn-outline-light" name="btn-logout" value="btn-logout">ログアウト</button>
+                <input type="hidden" name="transition" value="trans_main"></input>
+            </form>
+        </div>
--- page_entrance.php
+++ page_entrance.php
@@ -1,3 +1,93 @@
 <?php
+$passlist = array('hogehoge' => 'hogepass', 'hoge2' => 'hoge2pass');
 
+if (!array_key_exists('user', $_POST)) {
+    echoAuthPage("ログイン");
+    exit;
+}
+
+$user = $_POST['user'];
+$pass = $_POST['pass'];
+
+if (!array_key_exists($user, $passlist) || $passlist[$user] != $pass) {
+    echoAuthPage("パスワードが違います");
+    exit;
+}
+
+echoHelloPage($user);
+
+function echoAuthPage($msg)
+{
+    echo <<<EOT
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>ページタイトル</title>
+    </head>
+    <body>
+        $msg
+        <form method="POST" action="page_entrance.php">
+            username <input type="text" name="user" value=""><br>
+            password <input type="password" name="pass" value=""><br>
+            <button type="submit" name="login" value="login">Login</button>
+        </form>
+    </body>
+    </html>
+EOT;
+}
+
+function echoHelloPage($who)
+{
+    include 'head.php';
+
+    // Database connection and query
+    $hostname = '127.0.0.1';
+    $username = 'root';
+    $password = 'dbpass';
+    $dbname = 'task9';
+    $tablename = 'bbs';
+
+    $link = mysqli_connect($hostname, $username, $password);
+
+    if (!$link) {
+        exit("Connect error!");
+    }
+
+    $result = mysqli_select_db($link, $dbname);
+
+    if (!$result) {
+        exit("Use error on table ($dbname)!");
+    }
+
+    $selectQuery = "SELECT * FROM `$tablename`";
+    $result = mysqli_query($link, $selectQuery);
+
+    if ($result) {
+        // Display results
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "<hr>";
+            echo "<div class='post'>";
+            echo "<p>" . htmlspecialchars($row['id']) . "</p>";
+            echo "<h5>投稿者: " . htmlspecialchars($row['name']) . "</h5>";
+            echo "<p class='post-text'>" . htmlspecialchars($row['context']) . "</p>";
+            echo '<div class="container">';
+            echo "<p class='post-time'>投稿時間: " . htmlspecialchars($row['d_when']) . "</p>";
+            echo "<p type='button' class='likes' data-post-id='{$row['id']}' data-likes='{$row['good']}'>いいね数: <span class='like-count'>" . htmlspecialchars($row['good']) . "</span></p>";
+            echo "</div>";
+            echo "</div>";
+            echo "<hr>";
+        }
+    } else {
+        echo "Error fetching records: " . mysqli_error($link);
+    }
+
+    mysqli_free_result($result);
+    mysqli_close($link);
+
+    echo <<<EOT
+    </body>
+    </html>
+EOT;
+}
 ?>
